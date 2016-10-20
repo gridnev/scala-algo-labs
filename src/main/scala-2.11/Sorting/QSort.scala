98f3f4c31cca4f9d4dd94b5f356cd8d9303f78ed
dgridnev <denis.gridnev@gmail.com>
2016-10-20 09:22:18 -0700

    qsort finished

--- src/main/scala-2.11/Sorting/QSort.scala
+++ src/main/scala-2.11/Sorting/QSort.scala
@@ -1,24 +1,17 @@
 package Sorting
+import scala.util._
 
 /**
   * Created by denver on 10/19/16.
   */
 object QSort {
-  def sort(list:List[Int]):List[Int]= {
-    def split:(List[Int], List[Int]) = {
-      def getBase = scala.util.Random.nextInt(list.size)
-
-      val base = list(getBase)
-      list.partition(_ < base)
+  def sort(list: List[Int]): List[Int] =
+    if (list.size < 2)
+      list
+    else {
+      val base = list(Random.nextInt(list.size))
+      sort(list.filter(_ < base)) ++
+        list.filter(_ == base) ++
+        sort(list.filter(_ > base))
     }
-
-    list match {
-      case Nil => list
-      case x::Nil => list
-      case x::xs => {
-        val (left, right) = split
-        sort(left) ++ sort(right)
-      }
-    }
-  }
 }
