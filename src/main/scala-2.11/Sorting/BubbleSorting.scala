a7cc9c4cbf3acd9430b0e1d9f485ab4e2f1f2ff9
denver <denis.gridnev@gmail.com>
2016-10-20 13:42:19 -0700

    refactoring BubbleSort

--- src/main/scala-2.11/Sorting/BubbleSorting.scala
+++ src/main/scala-2.11/Sorting/BubbleSorting.scala
@@ -3,6 +3,21 @@ package Sorting
 /**
   * Created by denver on 10/20/16.
   */
-class BubbleSorting {
+object BubbleSorting {
+  def sort(list:List[Int]):List[Int] = {
+    def inner(l: List[Int]): (List[Int], Boolean) = {
+      def result(x: Int, t: (List[Int], Boolean)) = (x :: t._1, t._2)
 
+      l match {
+        case xs if xs.size < 2 => (xs, false)
+        case x0 :: x1 :: xs if x0 > x1 => result(x1, (inner(x0 :: xs)._1, true))
+        case x :: xs => result(x, inner(xs))
+      }
+    }
+
+    inner(list) match {
+      case (xs, false) => xs
+      case (xs, true) => sort(xs)
+    }
+  }
 }
